f9e8b8bdaeccfb06f9b50a8db65ec53a87470956
João Lenon <lenonsec7@gmail.com>
2020-05-26 22:58:16 -0300

    feat: CreateLike migration

---
+++ migrations/1590540786430-CreateLike.ts
@@ -0,0 +1,49 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm'
+
+export class CreateLike1590540786430 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'duo_likes',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'user_id_of_liked_user',
+            type: 'uuid',
+          },
+        ],
+      }),
+    )
+
+    await queryRunner.createForeignKey(
+      'duo_likes',
+      new TableForeignKey({
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'duo_users',
+        name: 'LikeUser',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('duo_likes')
+  }
+}
