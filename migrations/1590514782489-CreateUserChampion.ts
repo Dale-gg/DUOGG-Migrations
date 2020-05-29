16543c0830722a082174cc9b9918a27e7dd1388b
João Lenon <lenonsec7@gmail.com>
2020-05-29 09:13:33 -0300

    feat: add relations

--- migrations/1590514782489-CreateUserChampion.ts
+++ migrations/1590514782489-CreateUserChampion.ts
@@ -1,4 +1,9 @@
-import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm'
 
 export class CreateUserChampion1590514782489 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
@@ -22,6 +27,30 @@ export class CreateUserChampion1590514782489 implements MigrationInterface {
             type: 'uuid',
           },
         ],
+      }),
+    )
+
+    await queryRunner.createForeignKey(
+      'duo_users_champions_gg_champions',
+      new TableForeignKey({
+        columnNames: ['ggChampionsId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'gg_champions',
+        name: 'PivotUserChampion',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      }),
+    )
+
+    await queryRunner.createForeignKey(
+      'duo_users_champions_gg_champions',
+      new TableForeignKey({
+        columnNames: ['duoUsersId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'duo_users',
+        name: 'PivotUserUser',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
       }),
     )
   }
